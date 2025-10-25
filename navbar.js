3acf1d4addb8b4a6d76751a15a6b9dd2b1fb5ecd
TheDeGeO <loquisgg123@protonmail.com>
2025-10-24 23:11:30 -0600

    sections

--- navbar.js
+++ navbar.js
@@ -2,4 +2,5 @@ fetch('/navbar.html')
                 .then(response => response.text())
                 .then(data => {
                     document.getElementById('navbar-container').innerHTML = data;
+                    document.querySelector('.main-header').offsetHeight;
                 });
